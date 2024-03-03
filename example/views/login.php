9e5c62030c9f7901bae5fa218a582e9abb1a4924
Luca Cracco <craccoluca@gmail.com>
2024-03-03 19:10:37 +0100

    Fix example login.php page

--- example/views/login.php
+++ example/views/login.php
@@ -1,12 +1,27 @@
 <?php
+
+$method = 'GET';
+$idp = 'spid-demo';
+
 if (isset($_GET) && isset($_GET['selected_idp'])) {
     $idp = $_GET['selected_idp'];
+    $method = 'GET';
 }
 if (isset($_POST) && isset($_POST['selected_idp'])) {
     $idp = $_POST['selected_idp'];
+    $method = 'POST';
 }
 
-if (!$url = $sp->login($idp ?? 'spid-demo', 0, 1, 1, null, true)) {
+switch($method){
+  case 'GET':
+    $url = $sp->login($idp, 0, 1, 1, null, true);
+    break;
+  case 'POST':
+    $url = $sp->loginPost($idp, 0, 1, 1, null, true);
+    break;
+}
+
+if (!$url) {
     echo "Already logged in !<br>";
     echo "<a href=\"/\">Home</a>";
 } else {
